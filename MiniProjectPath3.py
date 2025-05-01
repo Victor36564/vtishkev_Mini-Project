786d5a632f587b4599fe0d7a5aad7c1997273830
Victor36564 <victish9@gmail.com>
2025-05-01 10:59:56 -0400

    Add files via upload

---
+++ MiniProjectPath3.py
@@ -0,0 +1,212 @@
+#MiniProjectPath3
+import numpy as np
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+#import models
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+import matplotlib.pyplot as plt
+from sklearn.decomposition import KernelPCA
+#import copy
+
+
+rng = np.random.RandomState(1)
+digits = datasets.load_digits()
+images = digits.images
+labels = digits.target
+
+#Get our training data
+X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.6, shuffle=False)
+
+def dataset_searcher(number_list,images,labels):
+  #insert code that when given a list of integers, will find the labels and images
+  #and put them all in numpy arrary (at the same time, as training and testing data)
+  indices = []
+
+  for i in range(len(labels)):
+    if labels[i] in number_list:
+      indices.append(i)
+
+  images_nparray = np.array(images)[indices]
+  labels_nparray = np.array(labels)[indices]
+
+  return images_nparray, labels_nparray
+
+def print_numbers(images,labels):
+  #insert code that when given images and labels (of numpy arrays)
+  #the code will plot the images and their labels in the title.
+  for i in range(10):
+    plt.imshow(images[i], cmap='gray')
+    plt.title(labels[i])
+    plt.show()
+
+class_numbers = [2,0,8,7,5]
+#Part 1
+class_number_images , class_number_labels = dataset_searcher(class_numbers, images, labels)
+#Part 2
+print_numbers(class_number_images , class_number_labels)
+
+
+model_1 = GaussianNB()
+
+#however, before we fit the model we need to change the 8x8 image data into 1 dimension
+# so instead of having the Xtrain data beign of shape 718 (718 images) by 8 by 8
+# the new shape would be 718 by 64
+X_train_reshaped = X_train.reshape(X_train.shape[0], -1)
+
+#Now we can fit the model
+model_1.fit(X_train_reshaped, y_train)
+#Part 3 Calculate model1_results using model_1.predict()
+X_test_reshaped = X_test.reshape(X_test.shape[0], -1)
+model1_results = model_1.predict(X_test_reshaped)
+
+
+def OverallAccuracy(results, actual_values):
+  #Calculate the overall accuracy of the model (out of the predicted labels, how many were correct?)
+  correct = np.sum(results == actual_values)
+  Accuracy = correct / len(actual_values)
+  return Accuracy
+
+
+# Part 4
+Model1_Overall_Accuracy = OverallAccuracy(model1_results, y_test)
+print("The overall results of the Gaussian model is " + str(Model1_Overall_Accuracy))
+
+#Part 5
+allnumbers = [0,1,2,3,4,5,6,7,8,9]
+allnumbers_images, allnumbers_labels = dataset_searcher(allnumbers, images, labels)
+
+allnumbers_images_reshaped = allnumbers_images.reshape(allnumbers_images.shape[0], -1)
+
+model1_all = model_1.predict(allnumbers_images_reshaped)
+print_numbers(allnumbers_images , model1_all)
+
+
+#Part 6
+#Repeat for K Nearest Neighbors
+model_2 = KNeighborsClassifier(n_neighbors=10)
+
+model_2.fit(X_train_reshaped, y_train)
+
+model2_results = model_2.predict(X_test_reshaped)
+
+Model2_Overall_Accuracy = OverallAccuracy(model2_results, y_test)
+print("The overall results of the K-NeighborsClassifier model is " + str(Model2_Overall_Accuracy))
+
+model2_all = model_2.predict(allnumbers_images_reshaped)
+print_numbers(allnumbers_images , model2_all)
+
+#Repeat for the MLP Classifier
+model_3 = MLPClassifier(max_iter=500, random_state=0)
+
+model_3.fit(X_train_reshaped, y_train)
+
+model3_results = model_3.predict(X_test_reshaped)
+
+Model3_Overall_Accuracy = OverallAccuracy(model3_results, y_test)
+print("The overall results of the MLPClassifier model is " + str(Model3_Overall_Accuracy))
+
+model3_all = model_3.predict(allnumbers_images_reshaped)
+print_numbers(allnumbers_images , model3_all)
+
+
+#Part 8
+#Poisoning
+# Code for generating poison data. There is nothing to change here.
+noise_scale = 10.0
+poison = rng.normal(scale=noise_scale, size=X_train.shape)
+
+X_train_poison = X_train + poison
+
+
+#Part 9-11
+#Determine the 3 models performance but with the poisoned training data X_train_poison and y_train instead of X_train and y_train
+X_train_poison_reshaped = X_train_poison.reshape(X_train_poison.shape[0], -1)
+
+model_1.fit(X_train_poison_reshaped, y_train)
+model1_results_poison = model_1.predict(X_test_reshaped)
+
+Model1_Overall_Accuracy_poison = OverallAccuracy(model1_results_poison, y_test)
+print("")
+print("The poisoned results of the Gaussian model is " + str(Model1_Overall_Accuracy_poison))
+
+model_2.fit(X_train_poison_reshaped, y_train)
+model2_results_poison = model_2.predict(X_test_reshaped)
+
+Model2_Overall_Accuracy_poison = OverallAccuracy(model2_results_poison, y_test)
+print("The poisoned results of the K-NeighborsClassifier model is " + str(Model2_Overall_Accuracy_poison))
+
+model_3.fit(X_train_poison_reshaped, y_train)
+model3_results_poison = model_3.predict(X_test_reshaped)
+
+Model3_Overall_Accuracy_poison = OverallAccuracy(model3_results_poison, y_test)
+print("The poisoned results of the MLPClassifier model is " + str(Model3_Overall_Accuracy_poison))
+
+
+#Part 12-13
+# Denoise the poisoned training data, X_train_poison.
+# hint --> Suggest using KernelPCA method from sklearn library, for denoising the data.
+# When fitting the KernelPCA method, the input image of size 8x8 should be reshaped into 1 dimension
+# So instead of using the X_train_poison data of shape 718 (718 images) by 8 by 8, the new shape would be 718 by 64
+kernel_pca = KernelPCA(
+    n_components=400,
+    kernel="rbf",
+    gamma=1e-3,
+    fit_inverse_transform=True,
+    alpha=5e-3,
+    random_state=42,
+)
+denoised_fit = kernel_pca.fit_transform(X_train_poison_reshaped)
+X_train_denoised = kernel_pca.inverse_transform(denoised_fit)
+
+X_train_denoised_reshaped = X_train_denoised.reshape(X_train_denoised.shape[0], -1)
+
+
+#Part 14-15
+#Determine the 3 models performance but with the denoised training data, X_train_denoised and y_train instead of X_train_poison and y_train
+#Explain how the model performances changed after the denoising process.
+
+model_1.fit(X_train_denoised_reshaped, y_train)
+model1_results_denoised = model_1.predict(X_test_reshaped)
+
+Model1_Overall_Accuracy_denoised = OverallAccuracy(model1_results_denoised, y_test)
+print("")
+print("The denoised results of the Gaussian model is " + str(Model1_Overall_Accuracy_denoised))
+
+model_2.fit(X_train_denoised_reshaped, y_train)
+model2_results_denoised = model_2.predict(X_test_reshaped)
+
+Model2_Overall_Accuracy_denoised = OverallAccuracy(model2_results_denoised, y_test)
+print("The denoised results of the K-NeighborsClassifier model is " + str(Model2_Overall_Accuracy_denoised))
+
+model_3.fit(X_train_denoised_reshaped, y_train)
+model3_results_denoised = model_3.predict(X_test_reshaped)
+
+Model3_Overall_Accuracy_denoised = OverallAccuracy(model3_results_denoised, y_test)
+print("The denoised results of the MLPClassifier model is " + str(Model3_Overall_Accuracy_denoised))
+
+# Function to display digits from the poisoned, denoised, and original training data sets
+# def print_numbers_comparison(noisy_images, denoised_images, original_images):
+#     denoised_images = denoised_images.reshape(-1,8,8)
+#     for i in range(10):
+#         # Show noisy image
+#         plt.imshow(noisy_images[i], cmap='gray')
+#         plt.title("Poisoned")
+#         plt.axis('off')
+#         plt.show()
+#
+#         # Show denoised image
+#         plt.imshow(denoised_images[i], cmap='gray')
+#         plt.title("Denoised")
+#         plt.axis('off')
+#         plt.show()
+#
+#         # Show original image
+#         plt.imshow(original_images[i], cmap='gray')
+#         plt.title("Original")
+#         plt.axis('off')
+#         plt.show()
+#
+# print_numbers_comparison(X_train_poison, X_train_denoised,X_train)
